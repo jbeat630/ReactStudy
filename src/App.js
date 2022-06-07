a3d50419e9491c155b55e75abe4e9970aeae6808
jbeat630 <light0630@nate.com>
2022-06-07 21:53:19 +0900

    useContext 공부해본거

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
+import { useState } from 'react'
 import './App.css';
-import State from './components/3-3.Hooks/State';
-import Reducer from './components/3-3.Hooks/Reducer';
-// import State from './components/selfStudy/State';
-// import ControlledComponent2 from './components/2-10.Form/ControlledComponent2';
-// import ControlledComponent from './components/2-10.Form/ControlledComponent';
-// import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
-// import List from './components/2-9.List/List';
-// import Condition from './components/2-8.ConditionalRendering/Condition';
-// import Event from './components/2-7.Event/Event';
-// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
-// import Extraction from './components/2-4.Props/Extraction/Extaction';
-// import Composition from './components/2-4.Props/Composition';
-// import ClassComponent from './components/2-5.State/ClassComponent';
-// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import './common/style.css';
+import Page from './components/selfStudy/useContext/Page';
+import { ThemeContext } from './components/selfStudy/useContext/context/ThemeContext';
+import {UserContext } from './components/selfStudy/useContext/context/UserContext';
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
     <div className="App">
-      <Reducer />
-      <State />
-      {/* <State /> */}
-      {/* <ControlledComponent2 /> */}
-      {/* <UncontrolledComponent /> */}
-      {/* <ControlledComponent /> */}
-      {/* <List /> */}
-      {/* <Condition /> */}
-      {/* <Event /> */}
-      {/* <ClassComponent2 /> */}
-      {/* <FunctionalComponent />
-      <ClassComponent />
-      <Extraction />
-      <Composition /> */}
+      <UserContext.Provider value={"사용자"}>
+        <ThemeContext.Provider value={{isDark, setIsDark}}>
+          <Page />
+        </ThemeContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
